8c0e21804553fb9a081ea8160aabdc67cdb5579d
mitryand <mitryand@umich.edu>
2022-01-28 16:30:45 -0500

    ready to test

--- TControl.py
+++ TControl.py
@@ -4,10 +4,18 @@ import time
 
 
 debug = True # set to False for final version
-motor_on = False
 motor_ID = 1
 
-control_modes = { 'duty' : 0, 'current_loop' : 1, 'current_break' : 2, 'velocity' : 3, 'position' : 4, 'set_origin' : 5, 'position_velocity_accel' : 6}
+control_modes = { 
+    'duty' : 0,
+    'current_loop' : 1,
+    'current_break' : 2,
+    'velocity' : 3, 
+    'position' : 4,
+    'set_origin' : 5, 
+    'position_velocity_accel' : 6
+    }
+    
 special_codes = {
     'enter_motor_control_mode' : [ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,0XFC ],
     'exit_motor_control_mode' : [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0XFD],
@@ -66,7 +74,7 @@ def parse_servo_message(message):
     temp = float(np.int8(data[6]))
     error = np.int8(data[7])
     motor = message.arbitration_id
-    
+
     if debug:
         print('motor: ' + str(motor))
         print('Position: ' + str(position))
@@ -76,6 +84,14 @@ def parse_servo_message(message):
         print('Error: ' + str(error))
 
     return (motor, position, speed, current, temp, error)
+
+def limit_value(value, max, min):
+    if value > max:
+        return max
+    elif value < min:
+        return min
+    else:
+        return value
 
     
 def power_on(bus, motor_id):
@@ -87,23 +103,17 @@ def power_off(bus, motor_id):
     
 
 def set_duty_cycle(bus, motor_id, duty):
-    if duty > 1.0:
-        duty = 1.0
-    elif duty < -1.0:
-        duty = -1.0
-
+    duty = limit_value(duty, -1.0, 1.0)
     duty_bytes = int(duty*100000.0).to_bytes()
     send_servo_control_message(bus, motor_id, control_modes['duty'], duty_bytes)
 
-
 def set_position_deg(bus, motor_id, position):
-    if position > 36000.0:
-        position = 36000.0
-    elif position < -36000.0:
-        position = -36000.0
-
+    position = limit_value(position, -36000.0, 36000.0)
     position_bytes = int(position * 1000000.0).to_bytes()
     send_servo_control_message(bus, motor_id, control_modes['position'], position_bytes)
+
+
+
 
 
 if __name__ == "__main__":
