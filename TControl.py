b9b6e60c7105c1afa21950a03d8faa661e6f4be3
mitryand <mitryand@umich.edu>
2022-02-07 12:39:28 -0500

    Updated to contain "MIT mode" functions

--- TControl.py
+++ TControl.py
@@ -2,9 +2,10 @@ import can
 import numpy as np
 import time
 
+# global settings
 
 debug = True # set to False for final version
-motor_ID = 1
+motor_ID = 2
 
 control_modes = { 
     'duty' : 0,
@@ -22,12 +23,36 @@ special_codes = {
     'zero_current_motor_position' : [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE]
 }
 
+Servo_Params = {
+    'Duty_min': -1.0,
+    'Duty_max': 1.0,
+    'I_min': -60.0,
+    'I_max': 60.0,
+    'V_min': -100000.0,
+    'V_max': 100000.0,
+    'P_min': -36000.0,
+    'P_max': 36000.0,
+    'A_min': -100000.0,
+    'A_max': 100000.0
+}
+
+MIT_Params = {
+    'P_min' : -95.5,
+    'P_max' : 95.5,
+    'V_min' : -30.0,
+    'V_max' : 30.0,
+    'I_min' : -18.0,
+    'I_max' : 18.0,
+    'Kp_min': 0.0,
+    'Kp_max': 500.0,
+    'Kd_min': 0.0,
+    'Kd_max': 5.0,
+}
+
+
+# Utility Functions
 
 def send_servo_control_message(bus, motor_id, control_mode, data):
-# Set up arbitration ID to contain the control mode in the proper place
-# Check that length of data isn’t more than 8 bytes (no buffer overflow attacks allowed!)
-# Try to send the CAN message
-# Return success or error
     DLC = len(data)
     assert (DLC <= 8), ('Data too long in message: ' + control_mode + " for motor: " + str(motor_id))
     arbitration_id = motor_id | (control_modes[control_mode] << 8 )
@@ -42,27 +67,6 @@ def send_servo_control_message(bus, motor_id, control_mode, data):
     except can.CanError:
         if debug:
             print("    Message NOT sent")
-
-def send_MIT_control_message(bus, motor_id, control_mode, data):
-# Set up arbitration ID to contain the control mode in the proper place
-# Check that length of data isn’t more than 8 bytes (no buffer overflow attacks allowed!)
-# Try to send the CAN message
-# Return success or error
-    DLC = len(data)
-    assert (DLC <= 8), ('Data too long in message: ' + control_mode + " for motor: " + str(motor_id))
-    arbitration_id = motor_id | (control_modes[control_mode] << 8 )
-    if debug:
-        print('ID: ' + str(hex(arbitration_id)) + '   Data: ' + '[{}]'.format(', '.join(hex(d) for d in data)) )
-    
-    message = can.Message(arbitration_id=arbitration_id, data=data, is_extended_id=False)
-    try:
-        bus.send(message)
-        if debug:
-            print("    Message sent on {}".format(bus.channel_info))
-    except can.CanError:
-        if debug:
-            print("    Message NOT sent")
-
 
 def send_setting_message(bus, motor_id, data):
     DLC = len(data)
@@ -82,10 +86,6 @@ def send_setting_message(bus, motor_id, data):
     
 
 def parse_servo_message(message):
-    # Takes in data from a CAN message
-    # Returns a tuple (position, speed, current, temp, error)
-    
-    # arbitration_id = message.arbitration_id
     data = bytes(message.data)
 
     if debug:
@@ -113,7 +113,8 @@ def parse_servo_message(message):
 
     return (motor, position, speed, current, temp, error)
 
-def limit_value(value, max, min):
+
+def limit_value(value, min, max):
     if value > max:
         return max
     elif value < min:
@@ -121,14 +122,9 @@ def limit_value(value, max, min):
     else:
         return value
 
-    
-def power_on(bus, motor_id):
-    send_setting_message(bus, motor_id, special_codes['enter_motor_control_mode'])
-        
 
-def power_off(bus, motor_id):
-    send_setting_message(bus, motor_id, special_codes['enter_motor_control_mode'])
     
+# Servo Mode Functions
 
 def set_duty_cycle(bus, motor_id, duty):
     duty = limit_value(duty, -1.0, 1.0)
@@ -141,33 +137,108 @@ def set_position_deg(bus, motor_id, position):
     send_servo_control_message(bus, motor_id, control_modes['position'], position_bytes)
 
 
+# MIT Mode Functions
+def power_on(bus, motor_id):
+    send_setting_message(bus, motor_id, special_codes['enter_motor_control_mode'])
+        
+def power_off(bus, motor_id):
+    send_setting_message(bus, motor_id, special_codes['exit_motor_control_mode'])
+
+def float_to_uint(x,x_min,x_max,num_bits):
+    x = limit_value(x,x_min,x_max)
+    span = x_max-x_min
+    # (x - x_min)*(2^num_bits)/span
+    return int((x- x_min)*( float((1<<num_bits)/span)) )
+
+def uint_to_float(x,x_min,x_max,num_bits):
+    span = x_max-x_min
+    # (x*span/(2^num_bits -1)) + x_min
+    return float(x*span/((1<<num_bits)-1) + x_min)
+
+def MIT_controller(bus, motor_id, position, velocity, Kp, Kd, I):
+
+    position_uint16 = float_to_uint(position, MIT_Params['P_min'], MIT_Params['P_max'],16)
+    velocity_uint12 = float_to_uint(velocity, MIT_Params['V_min'], MIT_Params['V_max'],12)
+    Kp_uint12 = float_to_uint(Kp, MIT_Params['Kp_min'], MIT_Params['Kp_max'],12)
+    Kd_uint12 = float_to_uint(Kd, MIT_Params['Kd_min'], MIT_Params['Kd_max'],12)
+    I_uint12 = float_to_uint(I, MIT_Params['I_min'], MIT_Params['I_max'],12)
+
+    data = [position_uint16 >> 8,
+        position_uint16 & 0x00FF,
+        (velocity_uint12) >> 4,
+        (velocity_uint12&0x00F<<4) | (Kp_uint12) >> 8,
+        (Kp_uint12&0x0FF),
+        (Kd_uint12) >> 4,
+        (Kd_uint12&0x00F<<4) | (I_uint12) >> 8,
+        (I_uint12&0x0FF)
+    ]
+
+    send_setting_message(bus, motor_id, data)
 
 
+def parse_MIT_message(message):
+    
+    data = bytes(message.data)
+    if(len(data) < 8):
+        print("Missing Data!!!")
+        return
+
+    else:
+        motor = data[0]
+        position = data[1] <<8 | data[2]
+        velocity = data[3] << 8 | data[4]&0xF0
+        current = data[4]&0x0F<<8 | data[5]
+        temp = data[6]
+        error = data[7]
+
+    position = uint_to_float(position, MIT_Params['P_min'], MIT_Params['P_max'], 16)
+    velocity = uint_to_float(velocity, MIT_Params['V_min'], MIT_Params['V_max'], 12)
+    current = uint_to_float(current, MIT_Params['I_min'], MIT_Params['I_max'], 12)
+
+    if debug:
+        print(data)
+        print('  ID: ' + str(motor))
+        print('  Position: ' + str(position))
+        print('  Velocity: ' + str(velocity))
+        print('  Current: ' + str(current))
+        print('  Temp: ' + str(temp))
+        print('  Error: ' + str(error))
+
+    return (motor, position, velocity, current, temp, error)
+    
 
 if __name__ == "__main__":
 
-    bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
-    # try:
-    #     bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
-    # except:
-	#     print('Cannot find PiCAN board.')
+    p_des = 0.0
+    v_des = 0.0
+    i_des = 0.0
+    Kp = 1
+    Kd = 0.1
 
-    send_setting_message(bus, motor_ID, special_codes['enter_motor_control_mode'])
-    #set_position_deg(bus, motor_ID, 0.0)
-    time.sleep(0.5)
-    send_MIT_control_message(bus, motor_ID, 'position', [0,1,0,112,10,0,50])
+    try:
+        bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+    except:
+        print("Failed to open CAN bus.")
+    else:
 
-    time.sleep(0.25)
-
-    # send_servo_control_message(bus, motor_ID, 'position', [0,0,0,5])
- 
-    while True:
-        msg = bus.recv(timeout=1.0)
+        send_setting_message(bus, motor_ID, special_codes['enter_motor_control_mode'])
+        time.sleep(0.5)
         
-        if msg is not None:
-            (motor, position, speed, current, temp, error) = parse_servo_message(msg)
-        else:
-            print("timeout")
+        MIT_controller(bus, motor_ID, p_des, v_des, Kp, Kd, i_des)
+
+        timeout = 0.1
+        msg = bus.recv(timeout=1.0)
+        while True:
+            MIT_controller(bus, motor_ID, p_des, v_des, Kp, Kd, i_des)
+            msg_buffer = []
+            start = time.time()
+            while(msg != None and not (timeout < (time.time() - start))):
+                msg = bus.recv(timeout=1.0)
+                msg_buffer.append(msg)
+
+            if len(msg_buffer > 0):
+                for message in msg_buffer:
+                    parse_MIT_message(message)
 
 
 
