ebce5fd46920073205e873d5d6c656a4c49bc9d7
mitryand <mitryand@umich.edu>
2022-04-18 12:54:09 -0400

    some data processing

--- src/TMotorCANControl/test/plot_adc_motor_dephy_log.py
+++ src/TMotorCANControl/test/plot_adc_motor_dephy_log.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import csv
 import numpy as np
 from scipy.signal import butter, lfilter, freqz
+from scipy import interpolate
 
 def butter_lowpass(cutoff, fs, order=5):
     return butter(order, cutoff, fs=fs, btype='low', analog=False)
@@ -20,7 +21,7 @@ current_motor = []
 speed_motor = []
 
 test_dir= "saved_logs/"
-log_dir="torque_vary_with_angle/"
+log_dir="torque_vary_with_angle/synchronized_trial_1/"
 name="test-opposing-motor-2"
 
 
@@ -49,8 +50,14 @@ with open(test_dir + log_dir + dephy_name + ".csv",'r') as fd:
             angle_dephy.append(float(row[1]))
         i += 1
 
-time_dephy = np.array(time_dephy)
-angle_dephy = np.array(angle_dephy)
+torque_motor = np.array(torque_motor)
+current_motor = np.array(current_motor)
+time = np.array(time)
+
+time_dephy = np.array(time_dephy) + 2.6
+angle_dephy = np.array(angle_dephy)*9/50
+angle_dephy_func = interpolate.interp1d(time_dephy, angle_dephy, kind='linear',fill_value="extrapolate")
+angle_dephy_adjusted = angle_dephy_func(time[time>2.6])
 
 torque_adc_adjusted = -np.array(torque_adc)
 # og_torque = [0.091*9*i for i in current_motor]
@@ -62,8 +69,7 @@ cutoff = 10.0  # desired cutoff frequency of the filter, Hz
 torque_adc_filtered = butter_lowpass_filter(torque_adc_adjusted, cutoff, fs, order).reshape(-1,)
 
 # torque_adc_filtered = np.array(torque_adc_filtered)
-torque_motor = np.array(torque_motor)
-current_motor = np.array(current_motor)
+
 
 # og_torque = np.array(og_torque)
 
@@ -89,12 +95,14 @@ print("Max i_motor: " + str(current_motor.max()))
 # plt.plot(time,og_torque,label="τ_unadjusted (max: "+ str(round(og_torque.max(),2)) + "Nm)" )
 # plt.plot(np.array(time),speed_motor,label="v")
 # plt.plot(np.array(time),current_qaxis,label="i_q (max: " + str(round(current_qaxis.max(),2)) + "A)")
-plt.plot(np.array(time),torque_adc_adjusted,label="τ_adc (max: " + str(round(torque_adc_adjusted.max(),2)) + "Nm)")
 # plt.plot(np.array(time),torque_adc_filtered,label="τ_adc (max: " + str(round(torque_adc_filtered.max(),2)) + "Nm)")
 # plt.plot(np.array(time),current_adc,label="i_q (max: " + str(round(current_adc.max(),2)) + "A)")
 
 # plt.plot(np.array(time),current_motor,label="i_motor (max: " + str(round(current_motor.max(),2)) + "A)")
 # plt.plot(np.array(time),torque_motor,label="τ_q (max: " + str(round(torque_motor.max(),2)) + "Nm)")
+
+plt.plot(np.array(time),torque_adc_adjusted,label="τ_adc (max: " + str(round(torque_adc_adjusted.max(),2)) + "Nm)")
+
 plt.title('Torque vs Time')
 plt.ylabel('Torque [Nm]')
 plt.xlabel('Time [s]')
@@ -103,7 +111,20 @@ plt.legend()
 
 plt.show()
 
-plt.savefig(test_dir + log_dir + name + "_combined.png")
+# plt.savefig(test_dir + log_dir + name + "_combined.png")
+# plt.clf()
+
+plt.plot(angle_dephy_adjusted,torque_adc_adjusted[time > 2.6],label="τ_adc (max: " + str(round(torque_adc_adjusted.max(),2)) + "Nm)")
+
+plt.title('Torque vs Angle')
+plt.ylabel('Torque [Nm]')
+plt.xlabel('Motor-side Angle [rad]')
+plt.grid(True)
+plt.legend()
+
+plt.show()
+
+# plt.savefig(test_dir + log_dir + name + "_combined.png")
 # plt.clf()
 
 
