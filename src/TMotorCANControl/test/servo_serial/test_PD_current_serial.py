5a76b527b8319d1aa3f2b6e64dcef33e52f79469
Mitry <mitrymanderson@gmail.com>
2022-11-14 17:57:16 +0000

    PD control from the pi might be too slow

---
+++ src/TMotorCANControl/test/servo_serial/test_PD_current_serial.py
@@ -0,0 +1,28 @@
+from sys import path
+path.append("/home/pi/TMotorCANControl/src/")
+from TMotorCANControl.Serial_manager_servo import *
+from TMotorCANControl.TMotorManager_servo_serial import *
+
+current = 0
+
+Pdes = 0
+Vdes = 0
+
+P = 0
+D = 1
+
+with TMotorManager_servo_serial(port = '/dev/ttyUSB0', baud=961200, motor_params=Servo_Params_Serial['AK80-9']) as dev:
+        loop = SoftRealtimeLoop(dt=0.01, report=True, fade=0.0)
+        dev.set_zero_position()
+        dev.update()
+        
+        dev.enter_current_loop_control()
+        
+        for t in loop:
+            current = P*(Pdes - dev.get_output_angle_radians()) + D*(Vdes - dev.get_output_velocity_radians_per_second())
+            
+            dev.set_current_qaxis_amps(current)
+            dev.update()
+            print("\r" + str(dev), end='')
+
+        
