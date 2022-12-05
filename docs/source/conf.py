2c1e018f29e3b98b30d00e40fa05cd67fce3747c
mitryand <mitryand@umich.edu>
2022-12-04 22:06:50 -0500

    updating conf.py for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, '/home/mitry/Projects/TMotorCANControl/src')
-sys.path.insert(0, '/src') # for read the docs auto build
+sys.path.insert(0, '/home/docs/checkouts/readthedocs.org/user_builds/tmotorcancontrol/checkouts/latest/src/') # for read the docs auto build
 
 
 # -- Project information -----------------------------------------------------
