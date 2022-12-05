52f28f534b120f049ab9b49bf7f3d6939bdcc4e8
Mitry Anderson <mitrymanderson@gmail.com>
2022-12-04 22:03:46 -0500

    fixed for readthe docs?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, '/home/mitry/Projects/TMotorCANControl/src')
+sys.path.insert(0, '/src') # for read the docs auto build
 
 
 # -- Project information -----------------------------------------------------
